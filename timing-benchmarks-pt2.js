81aa27123db4bef6fe30b45afa9808dcf9661f79
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-06-28 15:35:04 -0700

    commit

--- timing-benchmarks-pt2.js
+++ timing-benchmarks-pt2.js
@@ -1,27 +1,20 @@
 const [addNums, addManyNums] = require("./timing-benchmarks-pt1.js");
 
 // Runs `addNums` in 10 increasing increments
-function addNums10(increment, count = 0) {
-  if (count === 10) return [];
-  let sum = 0;
-  for (let i = 1; i <= increment; i++)
-    sum += i;
-  count += 1
-  if (increment < 10) {
-    return [sum].concat(addNums10(increment + 1, count))
-  } else {
-    return [sum].concat(addNums10(increment + 10, count))
+function addNums10(increment) {
+  let sums = []
+  for(let n =0;n<=10* increment;n*= increment){
+    sums.push(addNums(n))
   }
+  return sums
 }
 // Runs `addManyNums` in 10 increasing increments
-function addManyNums10(increment) {
-  let length = increment >= 10 ? 100 : 10
-  let res = []
-  for (let i = increment; i <= length; i += increment < 10 ? 1 : 10) {
-    let thisRes = addNums10(increment)
+function addManyNums10(n) {
+  let sums = []
+  for (let n = 0; n <= 10 * increment; n *= increment) {
+    sums.push(addManyNums(n))
   }
-  console.log(res)
-  return res
+  return sums
 }
 
 
